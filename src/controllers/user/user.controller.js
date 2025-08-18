493afac71eaeec63ad44434906f0bb1081312f9d
Mirzokhidbek <mirodev@sju.ac.kr>
2025-08-18 13:15:26 +0900

    some updates on login and sigup

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
 const UserModel = require("../../models/user/user.model");
 const { genSalt, hash } = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 class UserController {
   static signUp = async (req, res) => {
@@ -25,6 +26,27 @@ class UserController {
     res
       .status(StatusCodes.CREATED)
       .json({ message: "User registered successfully!" });
+  };
+  static login = async (req, res) => {
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ message: "Invaid email or password" });
+    }
+    // Generate JWT Token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
   };
 }
 module.exports = UserController;
